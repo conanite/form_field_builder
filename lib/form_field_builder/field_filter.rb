c3615135257a3edaa82ec6963e1d7a57585dd61a
conanite <conan@conandalton.net>
2019-03-09 21:11:36 +0100

    field_filter: new #never_show field-filter helper

--- lib/form_field_builder/field_filter.rb
+++ lib/form_field_builder/field_filter.rb
@@ -6,4 +6,20 @@ module FormFieldBuilder::FieldFilter
     else @filter_config.merge config
     end
   end
+
+  class FilterByNever < Struct.new(:key)
+    def to_s          ; "never | #{key.inspect}" ; end
+    def show?(*)      ; false                    ; end
+  end
+
+  # useful for subclasses with differing configs to share a single #form view
+  # example (avoid showing #blackmail and #extortion fields in an innocent subclass)
+  #
+  # class InnocentFoo < AbstractFoo
+  #   filter_config never_filter :blackmail, :extortion
+  # end
+  #
+  def never_show *names
+    names.each_with_object({}) { |name, hsh| hsh[name.to_sym] = FilterByNever.new(name) }
+  end
 end
