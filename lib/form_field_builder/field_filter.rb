bd635d9dae2d92ecbc9aa33ac8efe35c86076df8
conanite <conan@conandalton.net>
2020-08-17 17:21:46 +0200

    field_filter: introduce #replace_filter_config to override config from a superclass

--- lib/form_field_builder/field_filter.rb
+++ lib/form_field_builder/field_filter.rb
@@ -1,7 +1,11 @@
 module FormFieldBuilder::FieldFilter
-  def filter_config config=:not_set
+  def setup_filter_config
     @filter_config ||= superclass.filter_config.dup if superclass.respond_to?(:filter_config)
     @filter_config ||= FormFieldBuilder::FieldFilterConfig.new
+  end
+
+  def filter_config config=:not_set
+    setup_filter_config
 
     case config
     when :not_set; @filter_config
@@ -9,9 +13,19 @@ module FormFieldBuilder::FieldFilter
     end
   end
 
+  def replace_filter_config config
+    setup_filter_config
+    @filter_config.replace config
+  end
+
   class FilterByNever < Struct.new(:key)
     def to_s          ; "never | #{key.inspect}" ; end
     def show?(*)      ; false                    ; end
+  end
+
+  class FilterByAlways < Struct.new(:key)
+    def to_s          ; "always | #{key.inspect}" ; end
+    def show?(*)      ; true                      ; end
   end
 
   class FilterByProc < Struct.new(:name, :proc)
@@ -23,11 +37,15 @@ module FormFieldBuilder::FieldFilter
   # example (avoid showing #blackmail and #extortion fields in an innocent subclass)
   #
   # class InnocentFoo < AbstractFoo
-  #   filter_config never_filter :blackmail, :extortion
+  #   filter_config never_show :blackmail, :extortion
   # end
   #
   def never_show *names
     names.each_with_object({}) { |name, hsh| hsh[name.to_sym] = FilterByNever.new(name) }
+  end
+
+  def always_show *names
+    names.each_with_object({}) { |name, hsh| hsh[name.to_sym] = FilterByAlways.new(name) }
   end
 
   def proc_filter opts
--- lib/form_field_builder/field_filter_config.rb
+++ lib/form_field_builder/field_filter_config.rb
@@ -4,7 +4,16 @@ class FormFieldBuilder::FieldFilterConfig
   def initialize      ; @config = Hash.new { |h, k| h[k] = FormFieldBuilder::FilterChain.new } ; end
   def show? attr, obj ; config[attr.to_sym].show? obj                                          ; end
   def merge  more_cfg ;  more_cfg.each { |attr, filter| @config[attr] << filter }       ; self ; end
+
   def attr_to_s a, ff ; "#{a.to_s.ljust(20)}\n#{ff.map(&:to_s).join("\n") }"                   ; end
   def to_s            ; config.map { |attr, filters| attr_to_s attr, filters }.sort.join("\n") ; end
   def dup             ; self.class.new.tap { |d| config.each { |k,v| d.config[k] = v.dup } }   ; end
+
+  def replace new_cfg
+    new_cfg.each { |attr, filter|
+      @config[attr] = FormFieldBuilder::FilterChain.new
+      @config[attr] << filter
+    }
+    self
+  end
 end
